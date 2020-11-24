37818bac0fb99d55d8d8576df126a222262dba20
unknown <pdh0710@gmail.com>
2020-11-24 21:03:41 +0900

    Commit 2020-11-24_21-03-41

--- https/bmk_list.js
+++ https/bmk_list.js
@@ -85,7 +85,7 @@ var bmk = [
 		[1, "https://pdh0710.github.io/https/colors_uhd.html", "Color Bars Ultra Fine"],
 		[1, "https://pdh0710.github.io/https/colors_rotate.html", "불량화소 검사"],
 	],
-	[	[1, 'Service', 1, 0],
+	[	[1, 'Service Provider', 1, 0],
 		[0, "https://www.duckdns.org/", "Duck DNS"],
 		[0, "https://letsencrypt.org/", "Let's Encrypt"],
 		[0, "https://pages.github.com/", "GitHub Pages"],
