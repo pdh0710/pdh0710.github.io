ddf6e68424211499eba22cbbbf4ee67d853cc145
pdh0710 <pdh0710@gmail.com>
2023-08-06 00:52:30 +0900

    Commit 2023-08-06_00-52-30

--- https/bmk_list.js
+++ https/bmk_list.js
@@ -177,8 +177,7 @@ var bmk = [
 	[	[2, 'Daily', 1, 1],
 		[0, "http://www.cineast.co.kr/bbs/board.php?bo_table=psd_caption", "씨네스트 - 자막자료실"],
 		[0, "https://torrentqq268.com/torrent/mov.html", "토렌트큐큐 - 영화"],
-		[0, "https://1337x.to/trending/d/movies/", "1337X Trending Movies"],
-		[1, "https://1337x.to/trending/d/tv/", "TV"]
+		[0, "https://1337x.to/trending", "1337X Trending Today"]
 	],
 	[	[0, "Develope", 1, 0],
 		[0, "https://github.com/", "GitHub"],
