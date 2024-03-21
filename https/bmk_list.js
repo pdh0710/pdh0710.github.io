7f1fb9acc2a29167dbf54e7481459b584a5a3fab
Park, DongHwan <pdh0710@gmail.com>
2024-03-22 07:35:45 +0900

    2024.03.22 07:35

--- https/bmk_list.js
+++ https/bmk_list.js
@@ -180,12 +180,12 @@ var bmk = [
 	],
 	[	[1, "Torrent", 0, 0],
 		[0, "https://convert.jamack.net/", "Online 자막 변환기"],
+		[0, "https://bunco.co/", "번코"],
 		[0, "https://thepiratebay.org/", "The Pirate Bay"],
 		[0, "https://1337x.to/home/", "1337X : Verified Torrents"]
 	],
 	[	[2, 'Daily', 1, 1],
 		[0, "http://www.cineast.co.kr/bbs/board.php?bo_table=psd_caption", "씨네스트 - 자막자료실"],
-		[0, "https://bunco.co/", "번코"],
 		[0, "https://torrentqq302.com/torrent/mov.html", "토렌트큐큐 - 영화"],
 		[0, "https://1337x.to/trending", "1337X Trending Today"],
 		[1, "https://1337x.to/trending/d/movies/", "Movies"]
